68656e50df9a867aa555f27428a4d2c47f248e96
Michael Collins <40theridgeway@gmail.com>
2024-03-21 10:55:22 +0000

    Some changes for docker

--- src/main/java/com/waracle/cakemgr/CakeMManagerApplication.java
+++ src/main/java/com/waracle/cakemgr/CakeMManagerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootRestControllerUnitTestApplication {
+public class CakeMManagerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootRestControllerUnitTestApplication.class, args);
+		SpringApplication.run(CakeMManagerApplication.class, args);
 	}
 
 }
